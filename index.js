19ce2b794cb1e807494a7f9160797dc53c5ab0ae
Daniel Medina <daniel@medina.com>
2021-04-08 21:32:58 -0500

    Added /get-users route

--- index.js
+++ index.js
@@ -1,63 +1,71 @@
-require('dotenv').config()
-const nocache = require('nocache');
-const cors = require('cors');
-const faunadb = require('faunadb');
+require("dotenv").config();
+const nocache = require("nocache");
+const cors = require("cors");
+const faunadb = require("faunadb");
 const Query = faunadb.query;
-const express = require('express');
-const path = require('path');
-const {
-    join
-} = require('path');
+const express = require("express");
+const path = require("path");
+const { join } = require("path");
 
 const app = express();
 const port = process.env.PORT || 8080;
 const client = new faunadb.Client({
-    secret: process.env.FAUNADB
+  secret: process.env.FAUNADB,
 });
 
 const {
-    Get,
-    Map,
-    Lambda,
-    Documents,
-    Collection,
-    Paginate,
-    Var
-} = faunadb.query
+  Get,
+  Map,
+  Lambda,
+  Documents,
+  Collection,
+  Paginate,
+  Var,
+  Ref,
+} = faunadb.query;
 
 const demoConnection = client.query(
-    Map(
-        Paginate(
-            Documents(Collection("users"))
-        ),
-        Lambda("i", Get(Var("i")))
-    )
+  Map(Paginate(Documents(Collection("users"))), Lambda("i", Get(Var("i"))))
 );
+
+const userQuery = (id) => {
+  return client.query(Get(Ref(id)));
+};
 
 app.use(cors());
 app.use(nocache());
 
-app.get('/', (req, res) => {
-    res.sendFile(join(__dirname, 'public/index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(join(__dirname, "public/index.html"));
 });
 
-app.use('/static', express.static('public'));
+app.use("/static", express.static("public"));
 
-app.get('/get-users', (req, res) => {
-    demoConnection
-        .then(function (response) {
-            res.status(200).send(response);
-        })
-        .catch((err, data) => {
-            if (err) {
-                console.log(data);
-                res.status(500).send({
-                    "message": "Something went wrong on our end"
-                });
-            }
+app.get("/get-users", (req, res) => {
+  demoConnection
+    .then(function (response) {
+      res.status(200).send(response);
+    })
+    .catch((err, data) => {
+      if (err) {
+        console.log(data);
+        res.status(500).send({
+          message: "Something went wrong on our end",
         });
+      }
+    });
+});
+
+app.get("/get-user/:id", (req, res) => {
+  const query = client.query(Get(Ref(Collection("users"), req.params.id)));
+
+  query.then((response) => {
+    res.status(200).send({
+      response,
+    });
+  });
 });
 
 app.listen(port, () => {
-    console.log(`App listening on http://localhost:${port}`);
+  console.log(`App listening on http://localhost:${port}`);
 });
