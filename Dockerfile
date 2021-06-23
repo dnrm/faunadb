ea6c7ee7d5fc7817851233a09ca628b657b6e353
Daniel Medina <daniel@medina.com>
2021-06-23 14:29:53 -0500

    Updated port in index.js and Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM node:14.16.0
 WORKDIR /usr/app
 COPY . .
 RUN npm install --production
-ENV PORT=8080
+ENV PORT=80
+ENV NODE_ENV=production
 EXPOSE ${PORT}
 CMD [ "node", "index.js" ]
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { join } = require('path');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 80;
 const client = new faunadb.Client({
   secret: process.env.FAUNADB,
 });
